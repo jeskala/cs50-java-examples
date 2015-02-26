7dc4e4266732820c5f3d3d8d3185d2daf224b603
jeskala <jskala@aol.com>
2015-02-26 01:08:45 -0600

    adding in JavaCommandLine examples

--- src/JavaCommandLine.java
+++ src/JavaCommandLine.java
@@ -1,15 +1,13 @@
 import java.io.Console;
 
-
 public class JavaCommandLine {
 
     public static void main(String[] varArgs) {
-        System.out.println("Hello I am a computer");
+	System.out.println("Hello I am a computer");
 
-        Console c = System.console();
-        String name = c.readLine("What is your name?? ");
-        System.out.println("Hello " + name);
-
+	Console c = System.console();
+	String name = c.readLine("What is your name?? ");
+	System.out.println("Hello " + name);	
 
     }
 
